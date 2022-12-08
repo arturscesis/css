91f881c4b371205a595d21fcfceb30a6596d4a30
Arturs <arturspodzins21@gmail.com>
2022-12-09 01:25:00 +0200

    DONE

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>1 - Design</title>
+</head>
+<body>
+    
+    <div class="side">
+    <a href="other.php">next</a>
+        <div class="side-img-circle">
+            <img src="https://www.whitehouse.gov/wp-content/uploads/2021/01/45_donald_trump.jpg" style='height: 100%; width: 100%; object-fit: contain '>
+        </div>
+    </div>
+
+    <div class="first-1-cube"></div>
+    <div class="first-2-cube"></div>
+    <div class="first-3-cube"></div>
+
+    <div class="first-1-circle"></div>
+    <div class="first-2-circle"></div>
+
+    <div class="big-1-cube"></div>
+    <div class="big-2-cube"></div>
+    <div class="big-3-circle"><p>Virsraksts</p></div>
+    <div class="big-4-cube"></div>
+    <div class="big-5-cube"></div>
+
+    <div class="second-1-cube"></div>
+    <div class="second-2-cube"></div>
+    <div class="second-3-cube"></div>
+
+    <div class="second-1-circle"></div>
+    <div class="second-2-circle"></div>
+
+    <div class="last-1-cube">
+        <img src="https://wallpaperaccess.com/full/3723586.jpg" style='height: 100%; width:100%;'>
+    </div>
+    <div class="last-2-cube">
+        <img src="https://i.redd.it/m82rd2gephq61.jpg"style='height: 100%; width:100%;'>
+    </div>
+
+    <div class="last-3-cube">
+        <img src="https://www.coolthings.com/wp-content/uploads/2016/11/aston-martin-vanquish-s-2.jpg" style='height: 100%; width:100%;'>
+    </div>
+    <div class="last-4-cube">
+        <img src="https://cutewallpaper.org/21/cool-car-screensavers/Car-Wallpapers-in-4K-HD-for-your-desktop-phone-or-tablet-.jpg" style='height: 100%; width:100%;'>
+    </div>
+
+    <div class="last-5-cube">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvd8vXfhfrOUljjwLhHJ7lBJ0YECANKlz8iBsNHiPtSVEaGP67ROcEuQuY4fKySh8PbVE&usqp=CAU" style="height: 100%; width: 100%;">
+    </div>
+</body>
+</html>
---
+++ other.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>2 - Design</title>
+</head>
+<body>
+    <a href="index.php" style="z-index: 1;">back</a>
+    <div class="first-cube">
+        <div class="first-1-cube">
+            <div class="first-1-circle"></div>
+            <div class="first-2-circle"></div>
+        </div>
+        <div class="first-2-cube">
+            <div class="f-1-cube"></div>
+            <div class="f-2-cube"></div>
+        </div>
+        <div class="fi-1-circle"></div>
+        <div class="fi-2-circle"></div>
+        <div class="fi-3-circle"></div>
+    </div>
+
+    <div class="second-cube">
+
+    </div>
+
+    <div class="third-cube">
+        <div class="t-1-cube">
+            <div class="t-1-circle">
+            </div>
+            <div class="t-2-circle">
+
+            </div>
+            <div class="t-1-circle-cube"></div>
+        </div>
+        <div class="t-2-cube">
+            <div class="t-2-cube-c">
+
+            </div>
+            <div class="t-3-cube-c">
+                
+            </div>
+            <div class="t-4-cube-c">
+                
+            </div>
+            <div class="t-5-cube-c">
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="forth-cube">
+        <div class="f-cube">
+            <div class="in-cube">
+                <div class="in-circle"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="fifth-cube">
+        <div class="fif-1-cube"></div>
+        <div class="fif-2-cube"></div>
+        <div class="fif-3-cube"></div>
+
+        <div class="fif-1-circle"></div>
+        <div class="fif-2-circle"></div>
+    </div>
+
+    <div class="sixth-cube">
+        <div class="s-1-circle"></div>
+        <div class="s-2-circle"></div>
+    </div>
+</body>
+</html>
